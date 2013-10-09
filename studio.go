075f4d5986963de8e807095652b235de5fc872a0
Kristoffer Grönlund <krig@koru.se>
2013-10-09 17:48:31 +0200

    Button clicking

--- studio.go
+++ studio.go
@@ -3,23 +3,29 @@ package main
 import (
 	"github.com/krig/Go-SDL2/sdl"
 	"github.com/krig/Go-SDL2/ttf"
+	"log"
 )
 
 type Resources struct {
 	renderer *sdl.Renderer
 	TitleFont *ttf.Font
 	PlayButton *sdl.Texture
+	StopButton *sdl.Texture
 	LEDButton *sdl.Texture
 }
 
 func (r *Resources) Load(rend *sdl.Renderer) {
 	r.renderer = rend
 
-	r.TitleFont = ttf.OpenFont("data/GaroaHackerClubeBold.otf", 10)
+	r.TitleFont = ttf.OpenFont("data/GaroaHackerClubeBold.otf", 12)
 
-	playbutton := sdl.Load("data/testbutton.png")
+	playbutton := sdl.Load("data/play2.png")
 	r.PlayButton = rend.CreateTextureFromSurface(playbutton)
 	playbutton.Free()
+
+	stopbutton := sdl.Load("data/stop2.png")
+	r.StopButton = rend.CreateTextureFromSurface(stopbutton)
+	stopbutton.Free()
 
 	ledbutton := sdl.Load("data/testbutton_blank.png")
 	r.LEDButton = rend.CreateTextureFromSurface(ledbutton)
@@ -31,6 +37,94 @@ func (r *Resources) Free() {
 	r.PlayButton.Destroy()
 }
 
+type Screen struct {
+	Pane
+	TopBar *TopBar
+	F1 *Button
+	F2 *Button
+	Title *Label
+	Play *Button
+	Stop *Button
+
+	Canvas *CanvasPane
+	//Tracks *TrackPane
+	//Current *Pane
+}
+
+type InputStack struct {
+	lovers []MouseLover
+}
+
+func (stack *InputStack) Add(lover MouseLover) {
+	stack.lovers = append(stack.lovers, lover)
+}
+
+func (stack *InputStack) OnMouseMotionEvent(event *sdl.MouseMotionEvent) bool {
+	for _, l := range stack.lovers {
+		l.OnMouseMotionEvent(event)
+	}
+	return true
+}
+
+func (stack *InputStack) OnMouseButtonEvent(event *sdl.MouseButtonEvent) bool {
+	for _, l := range stack.lovers {
+		l.OnMouseButtonEvent(event)
+	}
+	return true
+}
+
+
+func (screen *Screen) Init(space sdl.Rect, rsc *Resources) {
+	topbarHeight := int32(32)
+	screen.Pos = space
+	screen.TopBar = &TopBar{}
+	screen.TopBar.Init(sdl.Rect{space.X, space.Y, space.W, topbarHeight})
+	screen.F1 = &Button{}
+	screen.F2 = &Button{}
+	screen.Title = &Label{}
+	screen.Play = &Button{}
+	screen.Stop = &Button{}
+
+	screen.TopBar.AddLeft(screen.F1)
+	screen.TopBar.AddLeft(screen.F2)
+	screen.TopBar.SetCenter(screen.Title)
+	screen.TopBar.AddRight(screen.Play)
+	screen.TopBar.AddRight(screen.Stop)
+
+	screen.F1.Init(rsc.renderer, sdl.Rect{space.X, space.Y, 1, 1}, rsc.LEDButton)
+	screen.F2.Init(rsc.renderer, sdl.Rect{screen.F1.Pos.X + screen.F1.Pos.W, space.Y, 1, 1}, rsc.LEDButton)
+	screen.Title.Init(rsc.renderer, sdl.Rect{screen.F2.Pos.X + screen.F2.Pos.W, space.Y, 1, topbarHeight}, "canvas mode", rsc.TitleFont, hexcolor(0x777777))
+	screen.Play.Init(rsc.renderer, sdl.Rect{screen.Title.Pos.X + screen.Title.Pos.W, space.Y, 1, 1}, rsc.PlayButton)
+	screen.Stop.Init(rsc.renderer, sdl.Rect{screen.Play.Pos.X + screen.Play.Pos.W, space.Y, 1, 1}, rsc.StopButton)
+
+	screen.F1.OnClick(func() {
+		log.Println("Canvas Mode clicked!")
+	})
+
+	screen.F2.OnClick(func() {
+		log.Println("Track Mode clicked!")
+	})
+
+	screen.Play.OnClick(func() {
+		log.Println("Play clicked!")
+	})
+
+	screen.Stop.OnClick(func() {
+		log.Println("Stop clicked!")
+	})
+
+	screen.AddVisual(screen.TopBar)
+	screen.AddLayout(screen.TopBar)
+
+	screen.Canvas = &CanvasPane{}
+	screen.Canvas.Pos = sdl.Rect{space.X, space.Y + topbarHeight, space.W, space.H - topbarHeight}
+	screen.AddVisual(screen.Canvas)
+	screen.AddLayout(screen.Canvas)
+
+	screen.UpdateLayout(space)
+}
+
+
 func run_studio(window *sdl.Window, rend *sdl.Renderer) {
 	rsc := &Resources{}
 	rsc.Load(rend)
@@ -40,6 +134,11 @@ func run_studio(window *sdl.Window, rend *sdl.Renderer) {
 	neww, newh := w, h
 	screen := &Screen{}
 	screen.Init(sdl.Rect{0, 0, int32(w), int32(h)}, rsc)
+	stack := &InputStack{}
+	stack.Add(screen.F1)
+	stack.Add(screen.F2)
+	stack.Add(screen.Play)
+	stack.Add(screen.Stop)
 	defer screen.Destroy()
 
 	event := &sdl.Event{}
@@ -54,6 +153,12 @@ func run_studio(window *sdl.Window, rend *sdl.Renderer) {
 				if e.Keysym.Keycode == sdl.K_ESCAPE {
 					running = false
 				}
+
+			case sdl.MouseMotionEvent:
+				stack.OnMouseMotionEvent(&e)
+
+			case sdl.MouseButtonEvent:
+				stack.OnMouseButtonEvent(&e)
 			}
 		}
 
@@ -64,8 +169,10 @@ func run_studio(window *sdl.Window, rend *sdl.Renderer) {
 			screen.UpdateLayout(sdl.Rect{0, 0, int32(w), int32(h)})
 		}
 
+		rend.SetDrawBlendMode(sdl.BLENDMODE_NONE)
 		rend.SetDrawColor(hexcolor(0x303030))
 		rend.Clear()
+		rend.SetDrawBlendMode(sdl.BLENDMODE_BLEND)
 		rend.SetDrawColor(hexcolor(0xffffff))
 		screen.Draw(rend)
 		rend.Present()
--- ui.go
+++ ui.go
@@ -17,6 +17,16 @@ type Layout interface {
 	UpdateLayout(space sdl.Rect)
 }
 
+type Clickable interface {
+	OnClick(handler func())
+}
+
+type MouseLover interface {
+	GetPos() sdl.Rect
+	OnMouseButtonEvent(event *sdl.MouseButtonEvent) bool
+	OnMouseMotionEvent(event *sdl.MouseMotionEvent) bool
+}
+
 type Widget struct {
 	Pos sdl.Rect
 }
@@ -29,16 +39,20 @@ type Pane struct {
 
 type Button struct {
 	Widget
-	// State: 0 = off, 1 = hover, 2 = on
+	// State: 0 = off, 1 = hover, 2 = on, 3 = on+hover
 	State int
 	texture *sdl.Texture
+	clickhandler func()
 }
 
 type Label struct {
 	Widget
 	Text string
 	Font *ttf.Font
+	Color sdl.Color
 	texture *sdl.Texture
+	texwidth int32
+	texheight int32
 }
 
 type HorizontalLayout struct {
@@ -46,6 +60,7 @@ type HorizontalLayout struct {
 	elements_left []Visual
 	element_center Visual
 	elements_right []Visual
+	HSpacing int32
 }
 
 type TopBar struct {
@@ -103,25 +118,30 @@ func (pane *Pane) Destroy() {
 	}
 }
 
-func (h *HorizontalLayout) UpdateLayout(space sdl.Rect) {
-	x := space.X
+func (h *HorizontalLayout) Init(space sdl.Rect) {
 	h.Pos = space
+	h.HSpacing = 2
+}
+
+func (h *HorizontalLayout) UpdateLayout(space sdl.Rect) {
+	x := space.X + h.HSpacing
+	h.Pos = sdl.Rect{space.X, space.Y, space.W, h.Pos.H}
 	for _, w := range h.elements_left {
 		pos := w.GetPos()
 		w.SetPos(sdl.Rect{x, space.Y, pos.W, pos.H})
-		x += pos.W
-	}
-	if h.element_center != nil {
-		pos := h.element_center.GetPos()
-		h.element_center.SetPos(sdl.Rect{x, space.Y, pos.W, pos.H})
+		x += pos.W + h.HSpacing
 	}
 
-	x = space.X + space.W
+	rx := space.X + space.W - h.HSpacing
 	for i := len(h.elements_right) - 1; i >= 0; i-- {
 		w := h.elements_right[i]
 		pos := w.GetPos()
-		w.SetPos(sdl.Rect{x - pos.W, space.Y, pos.W, pos.H})
-		x -= pos.W
+		w.SetPos(sdl.Rect{rx - pos.W, space.Y, pos.W, pos.H})
+		rx -= (pos.W + h.HSpacing)
+	}
+
+	if h.element_center != nil {
+		h.element_center.SetPos(sdl.Rect{x, space.Y, rx - x, h.Pos.H})
 	}
 }
 
@@ -150,6 +170,10 @@ func (h *HorizontalLayout) Destroy() {
 }
 
 func (tb *TopBar) Draw(rend *sdl.Renderer) {
+	rend.SetDrawColor(hexcolor(0x404040))
+	rend.FillRect(&tb.Pos)
+	rend.SetDrawColor(hexcolor(0xffffff))
+
 	for _, w := range tb.elements_left {
 		w.Draw(rend)
 	}
@@ -176,11 +200,12 @@ func (button *Button) Init(rend *sdl.Renderer, space sdl.Rect, texture *sdl.Text
 	button.State = 0
 }
 
-func (label *Label) Init(rend *sdl.Renderer, space sdl.Rect, text string, font *ttf.Font) {
+func (label *Label) Init(rend *sdl.Renderer, space sdl.Rect, text string, font *ttf.Font, color sdl.Color) {
 	label.Pos = space
 	label.Text = text
 	label.Font = font
 	label.texture = nil
+	label.Color = color
 	label.Update(rend)
 }
 
@@ -189,6 +214,38 @@ func (button *Button) Destroy() {
 		button.texture.Destroy()
 		button.texture = nil
 	}
+}
+
+func (button *Button) OnClick(handler func()) {
+	button.clickhandler = handler
+}
+
+func (button *Button) OnMouseMotionEvent(event *sdl.MouseMotionEvent) bool {
+	if button.Pos.Contains(event.X, event.Y) {
+		button.State |= 1
+	} else {
+		button.State &= ^1
+	}
+	return true
+}
+
+func (button *Button) OnMouseButtonEvent(event *sdl.MouseButtonEvent) bool {
+	if (event.Button == sdl.BUTTON_LEFT) {
+		contains := button.Pos.Contains(event.X, event.Y)
+		pressed := event.State == sdl.PRESSED
+		was_pressed := (button.State & 2) != 0
+		if pressed && contains {
+			button.State |= 2
+		} else if was_pressed && !pressed && contains {
+			button.State &= ^2
+			if button.clickhandler != nil {
+				button.clickhandler()
+			}
+		} else {
+			button.State &= ^2
+		}
+	}
+	return true
 }
 
 
@@ -208,70 +265,26 @@ func (label *Label) Update(rend *sdl.Renderer) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	txt_surface := label.Font.RenderText_Blended(label.Text, hexcolor(0xffffff))
+	txt_surface := label.Font.RenderText_Blended(label.Text, label.Color)
 	if label.texture != nil {
 		label.texture.Destroy()
 	}
 	label.texture = rend.CreateTextureFromSurface(txt_surface)
 	txt_surface.Free()
-	label.Pos.W = int32(textw)
-	label.Pos.H = int32(texth)
+	label.texwidth = int32(textw)
+	label.texheight = int32(texth)
 }
 
 func (button *Button) Draw(rend *sdl.Renderer) {
 	w := button.Pos.W
-	rend.Copy(button.texture, &sdl.Rect{int32(button.State) * w, 0, button.Pos.W, button.Pos.H}, &button.Pos)
+	state := button.State
+	if state > 2 {
+		state = 2
+	}
+	rend.Copy(button.texture, &sdl.Rect{int32(state) * w, 0, button.Pos.W, button.Pos.H}, &button.Pos)
 }
 
 func (label *Label) Draw(rend *sdl.Renderer) {
-	rend.Copy(label.texture, nil, &label.Pos)
+	pos := sdl.Rect{label.Pos.X + (label.Pos.W - label.texwidth) / 2, label.Pos.Y + (label.Pos.H - label.texheight)/2, label.texwidth, label.texheight}
+	rend.Copy(label.texture, nil, &pos)
 }
-
-type Screen struct {
-	Pane
-	TopBar *TopBar
-	F1 *Button
-	F2 *Button
-	Title *Label
-	Play *Button
-	Stop *Button
-
-	Canvas *CanvasPane
-	//Tracks *TrackPane
-	//Current *Pane
-}
-
-func (screen *Screen) Init(space sdl.Rect, rsc *Resources) {
-	topbarHeight := int32(32)
-	screen.Pos = space
-	screen.TopBar = &TopBar{}
-	screen.TopBar.Pos = sdl.Rect{space.X, space.Y, space.W, topbarHeight}
-	screen.F1 = &Button{}
-	screen.F2 = &Button{}
-	screen.Title = &Label{}
-	screen.Play = &Button{}
-	screen.Stop = &Button{}
-
-	screen.TopBar.AddLeft(screen.F1)
-	screen.TopBar.AddLeft(screen.F2)
-	screen.TopBar.SetCenter(screen.Title)
-	screen.TopBar.AddRight(screen.Play)
-	screen.TopBar.AddRight(screen.Stop)
-
-	screen.F1.Init(rsc.renderer, sdl.Rect{space.X, space.Y, 1, 1}, rsc.LEDButton)
-	screen.F2.Init(rsc.renderer, sdl.Rect{screen.F1.Pos.X + screen.F1.Pos.W, space.Y, 1, 1}, rsc.LEDButton)
-	screen.Title.Init(rsc.renderer, sdl.Rect{screen.F2.Pos.X + screen.F2.Pos.W, space.Y, 1, 1}, "canvas mode", rsc.TitleFont)
-	screen.Play.Init(rsc.renderer, sdl.Rect{screen.Title.Pos.X + screen.Title.Pos.W, space.Y, 1, 1}, rsc.PlayButton)
-	screen.Stop.Init(rsc.renderer, sdl.Rect{screen.Play.Pos.X + screen.Play.Pos.W, space.Y, 1, 1}, rsc.LEDButton)
-
-	screen.AddVisual(screen.TopBar)
-	screen.AddLayout(screen.TopBar)
-
-	screen.Canvas = &CanvasPane{}
-	screen.Canvas.Pos = sdl.Rect{space.X, space.Y + topbarHeight, space.W, space.H - topbarHeight}
-	screen.AddVisual(screen.Canvas)
-	screen.AddLayout(screen.Canvas)
-
-	screen.UpdateLayout(space)
-}
-
